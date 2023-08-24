cc85119f06226da8a976fa3fd49e3db75dc6fd9a
Kerimov056 <ulvisk@code.edu.az>
2023-08-24 12:29:26 -0700

    getReservComplatedCount companent

---
+++ src/views/FoCompanent/ComplatedReservation.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+    Card,
+    Row,
+    Col,
+} from "react-bootstrap";
+import { useQuery } from "react-query";
+import { getReservComplatedCount } from "../../Services/reservationServices";
+
+const ComplatedReservation = () => {
+
+    const { data: reservComlated } = useQuery({
+        queryKey: ["ReservCarCount"],
+        queryFn: getReservComplatedCount,
+        staleTime: 0,
+    });
+
+    return (
+        <>
+            <Col id='CCCC' lg="3" sm="6">
+                <Card className="card-stats">
+                    <Card.Body>
+                        <Row>
+                            <Col xs="5">
+                                <div className="icon-big text-center icon-warning">
+                                    <i className="nc-icon nc-app"></i>
+                                </div>
+                            </Col>
+                            <Col xs="7">
+                                <div className="numbers">
+                                    <p className="card-category">Reserved Cars</p>
+                                    <Card.Title as="h4">{reservComlated?.data == null ? 0 : reservComlated?.data}</Card.Title>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                    <Card.Footer>
+                        <hr></hr>
+                        <div className="stats">
+                            <i className="nc-icon nc-align-left-2"></i>
+                            Reservation Car
+                        </div>
+                    </Card.Footer>
+                </Card>
+            </Col>
+        </>
+    )
+}
+
+export default ComplatedReservation
+
