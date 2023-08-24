c437c3068eefeb35509bdd1a6a28e7e7381f5374
Kerimov056 <ulvisk@code.edu.az>
2023-08-24 09:07:44 -0700

    carCount

--- src/Services/carServices.js
+++ src/Services/carServices.js
@@ -4,6 +4,11 @@ export const getCar = () => {
   return httpClient.get("api/Car")
 };
 
+export const getCarCount = () => {
+  return httpClient.get("api/Car")
+};
+
+
 export const getByCar = (id) => {
   return httpClient.get(`api/Car/${id}`);
 };
--- src/views/CardCompanent.js
+++ src/views/CardCompanent.js
@@ -3,12 +3,22 @@ import {
     Card,
     Row,
     Col,
-  } from "react-bootstrap";
-  import "./cardcompanent.scss";
+} from "react-bootstrap";
+import "./cardcompanent.scss";
+import { useQuery } from "react-query";
+import { getCarCount } from "../Services/carServices";
+
 
 const CardCompanent = () => {
+
+    const { data: cars } = useQuery({
+        queryKey: ["CarCount"],
+        queryFn: getCarCount,
+        staleTime: 0,
+    });
+
     return (
-        <>  
+        <>
             <Col id='CCCC' lg="3" sm="6">
                 <Card className="card-stats">
                     <Card.Body>
@@ -21,7 +31,7 @@ const CardCompanent = () => {
                             <Col xs="7">
                                 <div className="numbers">
                                     <p className="card-category">Number</p>
-                                    <Card.Title as="h4">150GB</Card.Title>
+                                    <Card.Title as="h4">{cars?.data}</Card.Title>
                                 </div>
                             </Col>
                         </Row>
