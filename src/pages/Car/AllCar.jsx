9e92ad678e3e21ede4f2073b2fcc70d5b1695504
Kerimov056 <ulvisk@code.edu.az>
2023-08-24 14:46:44 -0700

    carcard

--- src/pages/Car/AllCar.jsx
+++ src/pages/Car/AllCar.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 import './allcar.scss'
-import Admin from 'layouts/Admin'
 import CarCard from './CarCard'
+import { useQuery } from "react-query";
+import { getCar } from "../../Services/carServices";
 
 const AllCar = () => {
+
+    const { data: allCars } = useQuery({
+        queryKey: ["CarCount"],
+        queryFn: getCar,
+        staleTime: 0,
+    });
+
+
     return (
         <>
             <Container>
                 <Row className='mt-5'>
+                    <CarCard />
+                    <CarCard />
+                    <CarCard />
+                    <CarCard />
+                    <CarCard />
                     <CarCard />
                 </Row>
             </Container>
