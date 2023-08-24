21a6cce61cb2aca09eb06d5e801e87189443727d
Kerimov056 <ulvisk@code.edu.az>
2023-08-24 10:25:35 -0700

    Notifications

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -17,6 +17,7 @@ import {
 } from "react-bootstrap";
 import CardCompanent from "./FoCompanent/CardCompanent";
 import ReservCar from "./FoCompanent/ReservCar";
+import Notifications from "./FoCompanent/Notifications";
 
 function Dashboard() {
   return (
@@ -28,32 +29,7 @@ function Dashboard() {
 
           <ReservCar />          
 
-          <Col lg="3" sm="6">
-            <Card className="card-stats">
-              <Card.Body>
-                <Row>
-                  <Col xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-vector text-danger"></i>
-                    </div>
-                  </Col>
-                  <Col xs="7">
-                    <div className="numbers">
-                      <p className="card-category">Errors</p>
-                      <Card.Title as="h4">23</Card.Title>
-                    </div>
-                  </Col>
-                </Row>
-              </Card.Body>
-              <Card.Footer>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock-o mr-1"></i>
-                  In the last hour
-                </div>
-              </Card.Footer>
-            </Card>
-          </Col>
+          <Notifications/>
 
           <Col lg="3" sm="6">
             <Card className="card-stats">
---
+++ src/views/FoCompanent/Notifications.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+    Card,
+    Row,
+    Col,
+} from "react-bootstrap";
+import { MdCircleNotifications } from "react-icons/md";
+import { useQuery } from "react-query";
+import { getReservCarCount } from "../../Services/carServices";
+
+const Notifications = () => {
+
+    const { data: reservCars } = useQuery({
+        queryKey: ["ReservCarCount"],
+        queryFn: getReservCarCount,
+        staleTime: 0,
+    });
+
+    return (
+        <>
+            <Col id='CCCC' lg="3" sm="6">
+                <Card className="card-stats">
+                    <Card.Body>
+                        <Row>
+                            <Col xs="5">
+                                <div className="icon-big text-center icon-warning">
+                                    <MdCircleNotifications />
+                                </div>
+                            </Col>
+                            <Col xs="7">
+                                <div className="numbers">
+                                    <p className="card-category">All Cars</p>
+                                    <Card.Title as="h4">{reservCars?.data == null ? 0 : reservCars?.data}</Card.Title>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                    <Card.Footer>
+                        <hr></hr>
+                        <div className="stats">
+                            <i className="far fa-calendar-alt mr-1"></i>
+                            Notifications
+                        </div>
+                    </Card.Footer>
+                </Card>
+            </Col>
+        </>
+    )
+}
+
+export default Notifications
+
