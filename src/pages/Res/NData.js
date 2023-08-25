69f91acb5b9d50d94cb4b643074e787124717134
Kerimov056 <ulvisk@code.edu.az>
2023-08-25 12:36:12 -0700

    Notifacitions Cancled

--- src/pages/Res/NData.js
+++ src/pages/Res/NData.js
@@ -2,25 +2,32 @@ import React from 'react'
 import { Button, Table } from 'react-bootstrap'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import { useQuery, useQueryClient } from "react-query";
-import { useParams, useHistory } from "react-router-dom";
-import { putReservConfirmed } from "../../Services/reservationServices";
+import { putReservConfirmed, putReservCancled } from "../../Services/reservationServices";
 
 const NData = (props) => {
 
     const queryClient = useQueryClient();
-    const navigate = useHistory();
 
     const handleConfirm = async (reservId) => {
         try {
             await putReservConfirmed(reservId);
             queryClient.invalidateQueries(["reservConform", reservId]);
             queryClient.invalidateQueries(["reservAllPedding"]);
-            navigate("/NotificationsReservation");
         } catch (error) {
             console.error("Error confirming reservation:", error);
         }
     };
 
+    
+    const handleCancled = async (reservId) => {
+        try {
+            await putReservCancled(reservId);
+            queryClient.invalidateQueries(["reservCancled", reservId]);
+            queryClient.invalidateQueries(["reservAllPedding"]);
+        } catch (error) {
+            console.error("Error confirming reservation:", error);
+        }
+    };
 
     return (
         <>
@@ -33,7 +40,7 @@ const NData = (props) => {
                     <td>{props.return}</td>
                     <Link to={`/ReservDetail/${props.Id}`}><td><Button>Details</Button></td></Link>
                     <td><Button onClick={() => handleConfirm(props.Id)} variant="success">Confirm</Button></td>
-                    <td><Button variant="danger">Cancel</Button></td>
+                    <td><Button onClick={() => handleCancled(props.Id)} variant="danger">Cancel</Button></td>
                 </tr>
             </tbody>
         </>
