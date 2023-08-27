dcf4cc0ab2f1af92a365e9b4c6c47514ffc4e03c
Kerimov056 <ulvisk@code.edu.az>
2023-08-26 17:59:41 -0700

    BlogCard

--- src/views/Maps.js
+++ src/views/Maps.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import { getBlog } from "../Services/blogServices";
 import { useQuery } from "react-query";
+import BlogCard from './FoCompanent/BlogCard';
 
 const Maps = () => {
 
@@ -21,17 +22,17 @@ const Maps = () => {
           <Col md="12">
             <div className='LxDrC'>
               <button onClick={() => setCreateSlider(!createSlider)} data-text="Awesome" class="buttonLXDC">
-                <span class="actual-text">&nbsp;Chauffeurss&nbsp;</span>
-                <span class="hover-textLCD" aria-hidden="true">&nbsp;Chauffeurss&nbsp;</span>
+                <span class="actual-text">&nbsp;Blog&nbsp;</span>
+                <span class="hover-textLCD" aria-hidden="true">&nbsp;Blog&nbsp;</span>
               </button>
             </div>
             <div>
-              <Button>Create Chauffeurss</Button>
+              <Button>Create Blog</Button>
             </div>
 
             <div className="Chauffersss">
               {getByBlog?.data?.map((blog, index) => (
-                <Chauffer key={index} Id={cheuf?.id} name={cheuf?.name} number={cheuf?.number} />
+                <BlogCard key={index} Id={blog?.id} title={blog?.title} desc={blog?.descrtion?.slice(0, 60)} />
               ))}
             </div>
 
