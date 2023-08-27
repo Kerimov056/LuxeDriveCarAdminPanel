301fc5c51b08251b647dfec22538f198cfa09114
Kerimov056 <ulvisk@code.edu.az>
2023-08-26 19:49:51 -0700

    Faqs services

--- src/Services/faqsServices.js
+++ src/Services/faqsServices.js
@@ -3,3 +3,22 @@ import { httpClient } from "../utils/HttpClient";
 export const getFaqs = () =>{
     return httpClient.get("api/Faqs")
   };
+
+  export const getByFaqs = (id) => {
+    return httpClient.get(`api/Faqs/${id}`);
+  };
+  
+  export const removeFaqs = (blogId) => {             //cars
+    return httpClient.delete(`api/Faqs/${blogId}`)
+  };
+  
+  export const postFaqs = (data) => {         //Create car
+    return httpClient.post("api/Faqs",data)
+  };
+  
+  
+  export const UpdateFaqs = (id,data) =>{
+    return httpClient.put(`/api/Faqs/${id}`,data)
+  };
+  
+  
