f348ac615a01b578ba0422d17e193a6e512e8ebd
Kerimov056 <ulvisk@code.edu.az>
2023-09-01 15:04:08 -0700

    PickUpmap and ReturnLocation

--- src/views/Maps.js
+++ src/views/Maps.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './map.scss'
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
@@ -6,6 +6,7 @@ import { useQuery } from "react-query";
 import { getReservNow } from "../Services/reservationServices";
 import { useMutation, useQueryClient } from "react-query";
 import Moment from "moment";
+import { Button } from '@chakra-ui/react';
 
 const markerIcon = new L.Icon({
     iconUrl: require("../assets/img/faces/download.png"),
@@ -13,8 +14,6 @@ const markerIcon = new L.Icon({
     iconAnchor: [17, 46],
     popupAnchor: [0, -46],
 });
-
-
 
 const Maps = () => {
     const position = [40.4093, 49.8671]
@@ -26,12 +25,14 @@ const Maps = () => {
         staleTime: 0,
     });
 
-
     const formatDateTime = (inputDate) => {
         const date = new Date(inputDate);
         const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-        return date.toLocaleDateString('de-DE', options); 
-      };
+        return date.toLocaleDateString('de-DE', options);
+    };
+
+    const [returnLocation, setReturnLocation] = useState(false);
+    const [selectedMarker, setSelectedMarker] = useState(null);
 
     return (
         <>
@@ -44,28 +45,72 @@ const Maps = () => {
 
                     {Reservmap?.data?.map((maps, index) => (
                         <Marker
-                            position={[maps?.pickupLocation?.latitude, maps?.pickupLocation?.longitude]}
+                            position={[
+                                returnLocation
+                                    ? maps?.returnLocation?.latitude
+                                    : maps?.pickupLocation?.latitude,
+                                returnLocation
+                                    ? maps?.returnLocation?.longitude
+                                    : maps?.pickupLocation?.longitude,
+                            ]}
                             key={index}
                             icon={markerIcon}
+                            eventHandlers={{
+                                click: () => {
+                                    setSelectedMarker(maps);
+                                    setReturnLocation(false);
+                                },
+                            }}
                         >
-                            <Popup>
+                            <Popup> 
                                 <p style={{ color: "purple" }}>Pickup Location</p>
+                                <Button
+                                    onClick={() => setReturnLocation(true)}
+                                    style={{ color: "black", cursor: "pointer" }}
+                                >
+                                    Go To Return Location {">"}
+                                </Button>
                                 <p> FullName: {maps?.fullName}</p>
                                 <p> Email: {maps?.email}</p>
-                                <p> Pickup Date:  {formatDateTime(maps?.pickupDate)}</p>
-                                <img style={{width:"300px",height:"210px",objectFit:"cover"}} src={maps?.reservCar?.carImages[0]?.imagePath} />
+                                <p> Pickup Date: {formatDateTime(maps?.pickupDate)}</p>
+                                <img
+                                    style={{ width: "300px", height: "210px", objectFit: "cover" }}
+                                    src={maps?.reservCar?.carImages[0]?.imagePath}
+                                />
                             </Popup>
                         </Marker>
                     ))}
 
+                    {selectedMarker===true && (
+                        <Marker
+                            position={[
+                                selectedMarker?.returnLocation?.latitude,
+                                selectedMarker?.returnLocation?.longitude,
+                            ]}
+                            icon={markerIcon}
+                        >
+                            <Popup>
+                                <p style={{ color: "purple" }}>Pickup Location</p>
+                                <Button backgroundColor={"white"}
+                                    onClick={() => setReturnLocation(false)}
+                                    style={{ color: "white", cursor: "pointer" }}
+                                >
+                                    Go To Pickup Location {">"}
+                                </Button>
+                                <p> FullName: {selectedMarker?.fullName}</p>
+                                <p> Email: {selectedMarker?.email}</p>
+                                <p> Pickup Date: {formatDateTime(selectedMarker?.returnDate)}</p>
+                                <img
+                                    style={{ width: "300px", height: "210px", objectFit: "cover" }}
+                                    src={selectedMarker?.reservCar?.carImages[0]?.imagePath}
+                                />
+                            </Popup>
+                        </Marker>
+                    )}
                 </MapContainer>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Maps
-
-
-
-
+export default Maps;
