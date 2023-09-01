7066078e8959f17fc66339f57c1d8c899b5f7c0d
Kerimov056 <ulvisk@code.edu.az>
2023-09-01 14:18:22 -0700

    PickUpmap

--- src/views/Maps.js
+++ src/views/Maps.js
@@ -1,16 +1,37 @@
-
-
-
-
-
-
 import React from 'react'
 import './map.scss'
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
+import { useQuery } from "react-query";
+import { getReservNow } from "../Services/reservationServices";
+import { useMutation, useQueryClient } from "react-query";
+import Moment from "moment";
+
+const markerIcon = new L.Icon({
+    iconUrl: require("../assets/img/faces/download.png"),
+    iconSize: [45, 35],
+    iconAnchor: [17, 46],
+    popupAnchor: [0, -46],
+});
+
+
 
 const Maps = () => {
     const position = [40.4093, 49.8671]
+    const queryClient = useQueryClient();
+
+    const { data: Reservmap, isError } = useQuery({
+        queryKey: ["ReservMap"],
+        queryFn: getReservNow,
+        staleTime: 0,
+    });
+
+
+    const formatDateTime = (inputDate) => {
+        const date = new Date(inputDate);
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+        return date.toLocaleDateString('de-DE', options); 
+      };
 
     return (
         <>
@@ -21,11 +42,22 @@ const Maps = () => {
                         url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=S3UF58mBkVoHt2UkKpEL"
                     />
 
-                    <Marker position={position}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
+                    {Reservmap?.data?.map((maps, index) => (
+                        <Marker
+                            position={[maps?.pickupLocation?.latitude, maps?.pickupLocation?.longitude]}
+                            key={index}
+                            icon={markerIcon}
+                        >
+                            <Popup>
+                                <p style={{ color: "purple" }}>Pickup Location</p>
+                                <p> FullName: {maps?.fullName}</p>
+                                <p> Email: {maps?.email}</p>
+                                <p> Pickup Date:  {formatDateTime(maps?.pickupDate)}</p>
+                                <img style={{width:"300px",height:"210px",objectFit:"cover"}} src={maps?.reservCar?.carImages[0]?.imagePath} />
+                            </Popup>
+                        </Marker>
+                    ))}
+
                 </MapContainer>
             </div>
         </>
