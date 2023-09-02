65379418a36f4f160fcaaa37c32e1cf79d3804a3
Kerimov056 <ulvisk@code.edu.az>
2023-09-02 05:09:23 -0700

    Slıder Create

--- src/views/FoCompanent/Advantagecard.jsx
+++ src/views/FoCompanent/Advantagecard.jsx
@@ -1,137 +1,170 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useQuery, useQueryClient, useMutation } from "react-query";
-import { useParams, useHistory } from "react-router-dom";
-import { Formik, Field } from "formik";
-import { removeAdvatages, byAdvatages } from "../../Services/advantageServices";
-import "./advantagecard.scss";
-import { removeFaqs, UpdateFaqs } from "../../Services/faqsServices";
+import { useQuery, useQueryClient, useMutation } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { Formik, Field } from 'formik';
+import { removeAdvatages, byAdvatages } from '../../Services/advantageServices';
+import { removeFaqs, UpdateFaqs } from '../../Services/faqsServices';
+import './advantagecard.scss';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Advantagecard = (props) => {
-
     const [editAdvantages, setEditAdvantages] = useState(false);
     const [editFaqs, setEditFaqs] = useState(false);
 
-
     const { id } = useParams();
     const queryClient = useQueryClient();
-    const navigate = useHistory();
 
-    const { data: advantag } = useQuery(["advantages", id], () =>
-        byAdvatages(id)
-    );
+    const { data: advantag } = useQuery(['advantages', id], () => byAdvatages(id));
 
     const handleRemove = async (advantagesId) => {
         try {
             await removeAdvatages(advantagesId);
             queryClient.invalidateQueries(['AdvantagesRemove', advantagesId]);
-            queryClient.invalidateQueries(['getAllAdvatages']);
+            queryClient.invalidateQueries(['getAllAdvantages']);
         } catch (error) {
             console.error('Error removing advantages:', error);
         }
-    }
+    };
+
     const handleRemoveFaqs = async (faqsId) => {
         try {
             await removeFaqs(faqsId);
-            queryClient.invalidateQueries(["FaqsRemove", faqsId]);
-            queryClient.invalidateQueries(["getFaqs"]);
+            queryClient.invalidateQueries(['FaqsRemove', faqsId]);
+            queryClient.invalidateQueries(['getFaqs']);
         } catch (error) {
-            console.error("Error confirming reservation:", error);
+            console.error('Error confirming reservation:', error);
         }
-    }
-    const updateMutation = useMutation(Advantagecard, {
+    };
+
+    const updateAdvantageMutation = useMutation((updatedData) => UpdateAdvantage(id, updatedData), {
         onSuccess: () => {
-            queryClient.invalidateQueries(["advantages", id]);
+            queryClient.invalidateQueries(['advantages', id]);
+            setEditAdvantages(false);
         },
         onError: (error) => {
-            console.error("Error updating category:", error);
+            console.error('Error updating advantages:', error);
         },
-    })
-    const handleSubmit = async (values) => {
-        updateMutation.mutate({ ...advantag, ...values });
-    }
+    });
+
+    const handleAdvantageSubmit = async (values) => {
+        updateAdvantageMutation.mutate({ ...advantag, ...values });
+    };
+
     const mutation = useMutation((updatedData) => UpdateFaqs(props.Id, updatedData), {
         onSuccess: () => {
-            queryClient.invalidateQueries("getFaqs");
+            queryClient.invalidateQueries('getFaqs');
+            setEditFaqs(false);
         },
-    })
+    });
+
     const handleUpdate = async (values) => {
         try {
             await mutation.mutateAsync(values);
         } catch (error) {
-            console.error("Error updating FAQ:", error);
+            console.error('Error updating FAQ:', error);
         }
-    }
-    console.log(props.Id);
+    };
 
     return (
         <>
             <tbody>
                 <tr>
                     <td style={{ display: 'none' }}>{props.dto}</td>
-                    <td >{props.number}</td>
-                    <td >{props.title}</td>
-                    <td style={props.berirleme == 1 ? {} : { display: "none" }}  className='Slidersss'><Button style={{ backgroundColor: "yellow" }} ><Link to={`/AdvantagesDetails/${props.Id}`}>Details</Link></Button></td>
-                    <td style={props.berirleme == 2 ? {} : { display: "none" }}  className='Slidersss'><Button style={{ backgroundColor: "yellow" }} ><Link to={`/FaqsDetails/${props.Id}`}>Details</Link></Button></td>
-                    <td className='Artirrr' style={props.berirleme == 1 ? {} : { display: "none" }}  ><Button onClick={() => setEditAdvantages(!editAdvantages)} variant="primary">Edit</Button></td>
-                    <td className='Artirrr' style={props.berirleme == 2 ? {} : { display: "none" }} ><Button onClick={() => setEditFaqs(!editFaqs)} variant="primary">Edit</Button></td>
-                    <td style={props.berirleme == 1 ? {} : { display: "none" }} className='Artirrr'><Button onClick={() => handleRemove(props.Id)} variant="danger">Remove</Button></td>
-                    <td style={props.berirleme == 2 ? {} : { display: "none" }} className='Artirrr'><Button onClick={() => handleRemoveFaqs(props.FaqId)} variant="danger">Remove</Button></td>
+                    <td>{props.number}</td>
+                    <td>{props.title}</td>
+                    <td style={props.berirleme === 1 ? {} : { display: 'none' }} className="Slidersss">
+                        <Button style={{ backgroundColor: 'yellow' }}>
+                            <Link to={`/AdvantagesDetails/${props.Id}`}>Details</Link>
+                        </Button>
+                    </td>
+                    <td style={props.berirleme === 2 ? {} : { display: 'none' }} className="Slidersss">
+                        <Button style={{ backgroundColor: 'yellow' }}>
+                            <Link to={`/FaqsDetails/${props.Id}`}>Details</Link>
+                        </Button>
+                    </td>
+                    <td className="Artirrr" style={props.berirleme === 1 ? {} : { display: 'none' }}>
+                        <Button onClick={() => setEditAdvantages(!editAdvantages)} variant="primary">
+                            Edit
+                        </Button>
+                    </td>
+                    <td className="Artirrr" style={props.berirleme === 2 ? {} : { display: 'none' }}>
+                        <Button onClick={() => setEditFaqs(!editFaqs)} variant="primary">
+                            Edit
+                        </Button>
+                    </td>
+                    <td style={props.berirleme === 1 ? {} : { display: 'none' }} className="Artirrr">
+                        <Button onClick={() => handleRemove(props.Id)} variant="danger">
+                            Remove
+                        </Button>
+                    </td>
+                    <td style={props.berirleme === 2 ? {} : { display: 'none' }} className="Artirrr">
+                        <Button onClick={() => handleRemoveFaqs(props.FaqId)} variant="danger">
+                            Remove
+                        </Button>
+                    </td>
                 </tr>
-                {editAdvantages == true ? <div style={{ height: "250px" }} id='SliderEdit'>
-                    <Formik
-                        initialValues={{
-                            title: props.title,
-                            description: props.description,
-                        }}
-                        onSubmit={handleSubmit}
-                    >
-                        <Form id='EditAdvantage'>
-                            <div>
-                                <label>Title</label>
-                                <Field type="text" name="title" placeholder="Advantages Title" />
-                            </div>
-                            <div>
-                                <label >Descrption</label>
-                                <Field
-                                    type="text"
-                                    name="description"
-                                    placeholder="Advantages Description"
-                                />
-                            </div>
-                            <Button type="submit">Update</Button>
-                        </Form>
-                    </Formik>
-                </div> :
+                {editAdvantages === true ? (
+                    <div style={{ height: '250px' }} id="SliderEdit">
+                        <Formik
+                            initialValues={{
+                                Title: advantag?.title,
+                                Descrption: advantag?.description,
+                            }}
+                            onSubmit={handleAdvantageSubmit}
+                        >
+                            <Form id="EditAdvantage">
+                                <div>
+                                    <label>Title</label>
+                                    <Field
+                                        type="text"
+                                        name="Title"
+                                        placeholder="Advantages Title"
+                                    />
+                                </div>
+                                <div>
+                                    <label>Descrption</label>
+                                    <Field
+                                        type="text"
+                                        name="Descrption"
+                                        placeholder="Advantages Description"
+                                    />
+                                </div>
+                                <Button type="submit">Update</Button>
+                            </Form>
+                        </Formik>
+                    </div>
+                ) : (
                     <></>
-                }
-                {editFaqs == true ? <div style={{ height: "250px" }} id='SliderEdit'>
-                    <Formik
-                        initialValues={{
-                            title: props.title,
-                            description: props.description,
-                        }}
-                        onSubmit={handleUpdate}
-                    >
-                        <Form>
-                            <div>
-                                <label>Title</label>
-                                <Field type="text" name="title" placeholder="FAQ Title" />
-                            </div>
-                            <div>
-                                <label>Description</label>
-                                <Field type="text" name="description" placeholder="FAQ Description" />
-                            </div>
-                            <button type="submit">Update</button>
-                        </Form>
-                    </Formik>
-                </div> :
+                )}
+                {editFaqs === true ? (
+                    <div style={{ height: '250px' }} id="SliderEdit">
+                        <Formik
+                            initialValues={{
+                                title: props.title,
+                                description: props.description,
+                            }}
+                            onSubmit={handleUpdate}
+                        >
+                            <Form>
+                                <div>
+                                    <label>Title</label>
+                                    <Field type="text" name="title" placeholder="FAQ Title" />
+                                </div>
+                                <div>
+                                    <label>Description</label>
+                                    <Field type="text" name="description" placeholder="FAQ Description" />
+                                </div>
+                                <button type="submit">Update</button>
+                            </Form>
+                        </Formik>
+                    </div>
+                ) : (
                     <></>
-                }
+                )}
             </tbody>
         </>
-    )
-}
+    );
+};
 
-export default Advantagecard
+export default Advantagecard;
--- src/views/TableList.js
+++ src/views/TableList.js
@@ -1,152 +1,164 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Badge,
   Button,
-  Card,
-  Navbar,
-  Nav,
-  Table,
   Container,
   Row,
   Col,
   Form,
+  Card,
+  Table,
 } from "react-bootstrap";
 import SliderCard from "./SliderCard";
 import { getSlider, postSlider } from "../Services/sliderServices";
 import { useQuery } from "react-query";
-import { useFormik } from "formik";
-import axios from 'axios';
-import { useMutation, useQueryClient } from "react-query";
+import axios from "axios";
+import { useQueryClient } from "react-query";
 
 function TableList(props) {
-
   const [createSlider, setCreateSlider] = useState(false);
-  const { data: getSliders, isError } = useQuery({
-    queryKey: ["getAllSlider"],
-    queryFn: getSlider,
+  const [showImage, setShowImage] = useState(null);
+  const [image, setImage] = useState(null);
+  const queryClient = useQueryClient();
+
+  const { data: getSliders, isError } = useQuery(["getAllSlider"], getSlider, {
     staleTime: 0,
   });
+
   if (isError) {
     return <div>Bir hata oluştu</div>;
   }
-
-
-
-  const queryClient = useQueryClient();
-  const [showImage, setShowImage] = useState(null);
-  const [image, setImage] = useState();
-
 
   const newSlider = {
     photo: image,
   };
 
+  const fileUploadHandler = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+    setShowImage(URL.createObjectURL(file));
+  };
+
   const CreateSlider = async (e) => {
     e.preventDefault();
-
-
+    
     const formData = new FormData();
-    for (const [key, value] of Object.entries(newSlider)) {
-      formData.append(key, value);
-    };
-
-
-    await axios.post(`https://localhost:7152/api/Sliders`, formData, {
-      headers: {
-        Accept: "*/*"
-      }
-    })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
+    formData.append("image", image); // "image" adını kullanarak FormData'ya ekleyin.
+    
+    try {
+      const response = await axios.post(`https://localhost:7152/api/Sliders`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Content-Type'ı doğru ayarlayın.
+        },
+        
       });
-
-  }
-
-  const fileUploadHandler = async (e) => {
-    const files = e.target.files[0];
-    setImage(files);
-    setShowImage(URL.createObjectURL(files));
+      console.log(response.data); // Sunucu yanıtını kontrol etmek için loglayın.
+      queryClient.invalidateQueries("getAllSlider");
+    } catch (error) {
+      console.error(error); // Hata durumunda hata mesajını loglayın.
+    }
   };
 
 
   return (
-    <>
-      <Container fluid>
-        <Row>
-          <div className='LxDrC'>
-            <button data-text="Awesome" class="buttonLXDC">
-              <span class="actual-text">&nbsp;Slider&nbsp;</span>
-              <span class="hover-textLCD" aria-hidden="true">&nbsp;Slider&nbsp;</span>
-            </button>
-          </div>
-          <div onClick={() => setCreateSlider(!createSlider)} style={{ marginBottom: "20px", marginLeft: "20px" }}>
-            <Button>Create Slider</Button>
-          </div>
-          {createSlider == true ?
-            <div style={{ width: "100%", height: "290px", display: "flex", alignItems: "center" }}>
-              <div style={{ width: "90%", height: "120px", display: "flex", justifyContent: "space-around", alignItems: "center" }}>
-
-                <form controlId="formFileMultiple" onSubmit={(e) => CreateSlider(e)} className="mb-3">
-                  <div style={{ width: "70%" }}>
-
-                    <Form.Label>Image:</Form.Label>
-                    <p>Image</p>
-                    {
-                      showImage !== null ?
-                        <img
-                          style={{
-                            width: "200px",
-                            height: "100px",
-                            marginBottom: "10px",
-                            borderRadius: "unset",
-                          }}
-                          src={showImage}
-                          alt="header image"
-                        /> : null
-                    }
-                    <Form.Control
-                      type="file"
-                      required
-                      onChange={(e) => fileUploadHandler(e)}
+    <Container fluid>
+      <Row>
+        <div className="LxDrC">
+          <button data-text="Awesome" class="buttonLXDC">
+            <span class="actual-text">&nbsp;Slider&nbsp;</span>
+            <span class="hover-textLCD" aria-hidden="true">
+              &nbsp;Slider&nbsp;
+            </span>
+          </button>
+        </div>
+        <div
+          onClick={() => setCreateSlider(!createSlider)}
+          style={{ marginBottom: "20px", marginLeft: "20px" }}
+        >
+          <Button>Create Slider</Button>
+        </div>
+        {createSlider && (
+          <div
+            style={{
+              width: "100%",
+              height: "290px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "90%",
+                height: "120px",
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <form controlId="formFileMultiple" onSubmit={CreateSlider} className="mb-3">
+                <div style={{ width: "70%" }}>
+                  <Form.Label>Image:</Form.Label>
+                  <p>Image</p>
+                  {showImage !== null && (
+                    <img
+                      style={{
+                        width: "200px",
+                        height: "100px",
+                        objectFit: "cover",
+                        marginBottom: "10px",
+                        borderRadius: "unset",
+                      }}
+                      src={showImage}
+                      alt="header image"
                     />
-                  </div>
-                  <p style={{ width: "20%", marginTop: "35px" }}>
-                    <Button type="submit" style={{ width: "140px" }} variant="success">
-                      Create
-                    </Button>
-                  </p>
-                </form>
-
-              </div>
-            </div> : <></>
-          }
-        </Row>
-        <Row>
-          <Col md="12">
-            <Card className="strpied-tabled-with-hover">
-              <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover table-striped">
-                  <thead>
-                    <tr>
-                      <th style={{ fontSize: "16px" }} className="border-0">Num</th>
-                      <th style={{ fontSize: "16px" }} className="border-0">Slider</th>
-                      <th style={{ fontSize: "19px" }} className="border-0">Edit</th>
-                      <th style={{ fontSize: "19px" }} className="border-0">Remove</th>
-                    </tr>
-                  </thead>
+                  )}
+                  <Form.Control
+                    type="file"
+                    required
+                    onChange={(e) => fileUploadHandler(e)}
+                  />
+                </div>
+                <p style={{ width: "20%", marginTop: "35px" }}>
+                  <Button type="submit" style={{ width: "140px" }} variant="success">
+                    Create
+                  </Button>
+                </p>
+              </form>
+            </div>
+          </div>
+        )}
+      </Row>
+      <Row>
+        <Col md="12">
+          <Card className="strpied-tabled-with-hover">
+            <Card.Body className="table-full-width table-responsive px-0">
+              <Table className="table-hover table-striped">
+                <thead>
+                  <tr>
+                    <th style={{ fontSize: "16px" }} className="border-0">
+                      Num
+                    </th>
+                    <th style={{ fontSize: "16px" }} className="border-0">
+                      Slider
+                    </th>
+                    <th style={{ fontSize: "19px" }} className="border-0">
+                      Edit
+                    </th>
+                    <th style={{ fontSize: "19px" }} className="border-0">
+                      Remove
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
                   {getSliders?.data?.map((bySlider, index) => (
                     <SliderCard key={index} number={index + 1} Id={bySlider.id} />
                   ))}
-                </Table>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
