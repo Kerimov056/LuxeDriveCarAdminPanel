d779f7bdbd278b4ef8f12d00c3482a8a5d368d0f
Kerimov056 <ulvisk@code.edu.az>
2023-09-02 02:03:39 -0700

    Blog Create page

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import { getBlog } from "../Services/blogServices";
-import { useQuery } from "react-query";
 import BlogCard from './FoCompanent/BlogCard';
+import { Field, useFormik, Form } from "formik";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import './FoCompanent/blogcard.scss'
 
 const Blog = () => {
-
   const [create, setCreate] = useState(false);
+  const [imageFields, setImageFields] = useState([{ files: [] }]);
+
+  const addImageField = () => {
+    setImageFields([...imageFields, { files: [] }]);
+  };
+
+  const removeImageField = (index) => {
+    const updatedFields = imageFields.filter((field, i) => i !== index);
+    setImageFields(updatedFields);
+  };
+
+  const handleFileChange = (event, index) => {
+    const updatedFields = [...imageFields];
+    updatedFields[index] = { ...updatedFields[index], files: event.target.files };
+    setImageFields(updatedFields);
+  };
+
 
   const { data: getByBlog, isError } = useQuery({
     queryKey: ["getBlogs"],
@@ -31,8 +48,12 @@ const Blog = () => {
               </button>
             </div>
             <div>
-              <Button>Create Blog</Button>
-              {
+              <Button onClick={()=> setCreate(!create)}>Create Blog</Button>
+              {create === true &&
+
+                <div>
+                </div>
+
 
               }
             </div>
@@ -45,9 +66,92 @@ const Blog = () => {
 
           </Col>
         </Row>
-      </Container>
+      </Container >
     </>
   )
 }
 
 export default Blog
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div>
+//                   <Form>
+//                     <Row>
+//                       <Col className="pr-1" md="5">
+//                         <Form.Group>
+//                           <Col md="12">
+//                             <Form.Group>
+//                               <label>Description</label>
+//                               <Form.Control
+//                                 cols="80"
+//                                 defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm inthat two seat Lambo."
+//                                 placeholder="Here can be your description"
+//                                 name="Description"
+//                                 // value={formik.values.Description}
+//                                 // onChange={formik.handleChange}
+//                                 rows="4"
+//                                 as="textarea"
+//                               ></Form.Control>
+//                             </Form.Group>
+//                           </Col>
+
+
+//                           <Col className="pr-" md="6">
+//                             {imageFields.map((field, index) => (
+//                               <div id='ImgUpload' key={index}>
+//                                 <Form.Group style={index === 0 ? { marginLeft: "-70px" } : {}} controlId={`formFileMultiple_${index}`} className="mb-3">
+//                                   <Form.Label>Car Images</Form.Label>
+//                                   <Form.Control type="file" multiple onChange={(event) => handleFileChange(event, index)} />
+//                                 </Form.Group>
+//                                 {index > 0 &&
+//                                   <Button onClick={() => removeImageField(index)}>-</Button>
+//                                 }
+//                               </div>
+//                             ))}
+//                             <Button onClick={addImageField}>+</Button>
+//                           </Col>
+
+//                           <Col md="12">
+//                             <Form.Group>
+//                               <label>Description</label>
+//                               <Form.Control
+//                                 cols="80"
+//                                 defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm inthat two seat Lambo."
+//                                 placeholder="Here can be your description"
+//                                 name="Description"
+//                                 // value={formik.values.Description}
+//                                 // onChange={formik.handleChange}
+//                                 rows="4"
+//                                 as="textarea"
+//                               ></Form.Control>
+//                             </Form.Group>
+//                           </Col>
+//                         </Form.Group>
+//                       </Col>
+//                     </Row>
+//                     <Button
+//                       className="btn-fill pull-right"
+//                       type="submit"
+//                       // onClick={formik.handleSubmit}
+//                       variant="success"
+//                     >
+//                       Create Car
+//                     </Button>
+//                     <div className="clearfix"></div>
+//                   </Form>
---
+++ src/views/FoCompanent/BlogCreate.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const BlogCreate = () => {
+    return (
+        <Container>
+            <Form onSubmit={(e) => CreateBlog(e)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <p>Image</p>
+                    {
+                        showImage !== null ?
+                            <img
+                                style={{
+                                    width: "200px",
+                                    height: "100px",
+                                    marginBottom: "10px",
+                                    borderRadius: "unset",
+                                }}
+                                src={showImage}
+                                alt="header image"
+                            /> : null
+                    }
+                    <Form.Control
+                        type="file"
+                        required
+                        onChange={(e) => fileUploadHandler(e)}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter Title"
+                        required
+                        onFocus={(e) => e.target.placeholder = ''}
+                        onBlur={(e) => e.target.placeholder = 'Enter Title'}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </Form.Group>
+
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                        type="text"
+                        placeholder="Enter Description"
+                        required
+                        onFocus={(e) => e.target.placeholder = ''}
+                        onBlur={(e) => e.target.placeholder = 'Enter Description'}
+
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Button variant="outline-primary" type="submit">
+                    Create
+                </Button>
+                <Link to="/blogTable">
+                    <Button variant="outline-dark" type="submit" className='mx-2'>
+                        Cancel
+                    </Button>
+                </Link>
+            </Form>
+        </Container>
+    )
+}
+
+export default BlogCreate
